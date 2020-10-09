dbc4949e9bba4e0755211c2d6a3e7a41d1c39526
Nutchanon Ninyawee <nutchanon@codustry.com>
2020-10-10 01:38:23 +0700

    add time

--- main copy.py
+++ main copy.py
@@ -1,14 +1,16 @@
 import zeep
 from zeep.transports import Transport
 from requests import Session, Request
+from codetiming import Timer
 
-session = Session()
-session.verify = False
-transport = Transport(session=session)
+with Timer(name="context manager"):
+    session = Session()
+    session.verify = False
+    transport = Transport(session=session)
 
-wsdl = 'https://rdws.rd.go.th/serviceRD3/vatserviceRD3.asmx?wsdl'
-client = zeep.Client(wsdl=wsdl, transport=transport)
+    wsdl = 'https://rdws.rd.go.th/serviceRD3/vatserviceRD3.asmx?wsdl'
+    client = zeep.Client(wsdl=wsdl, transport=transport)
 
-result = client.service.Service('anonymous', 'anonymous', '', "โฟลว์แอคเคาท์", 0,0,0)
-# result = client.service.Service('anonymous', 'anonymous', '0105558096348', "", 0,0,0)
-print(result)
+    # result = client.service.Service('anonymous', 'anonymous', '', "โฟลว์แอคเคาท์", 0,0,0)
+    result = client.service.Service('anonymous', 'anonymous', '0105558096348', "", 0,0,0)
+    print(result)
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import zeep
 from zeep.transports import Transport
 from requests import Session, Request
+from codetiming import Timer
 
-session = Session()
-session.verify = False
-transport = Transport(session=session)
+with Timer(name="context manager"):
+    session = Session()
+    session.verify = False
+    transport = Transport(session=session)
 
-wsdl = 'https://rdws.rd.go.th/serviceRD3/checktinpinservice.asmx?wsdl'
-client = zeep.Client(wsdl=wsdl, transport=transport)
+    wsdl = 'https://rdws.rd.go.th/serviceRD3/checktinpinservice.asmx?wsdl'
+    client = zeep.Client(wsdl=wsdl, transport=transport)
 
-result = client.service.ServiceTIN('anonymous', 'anonymous', '1419900489238')
-print(result)
+    result = client.service.ServiceTIN('anonymous', 'anonymous', '1419900489238')
+    print(result)
